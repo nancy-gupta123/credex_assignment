29fd92ce471ac26c86a60e36a8f723a9d46a9c2e
Priyanka <pg6142728@gmail.com>
2025-05-11 15:00:48 +0530

    finalCommit

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
 'use client'
-import ContactForm from '@/components/ContactForm';
-import Navbar from '@/components/Navbar1';
-import Hero from '@/components/Hero';
-import HowItWorks from '@/components/HowItWorks';
-import Testimonials from '@/components/Testimonials';
-import WhyChooseUs from '@/components/WhyChooseUs';
+import dynamic from 'next/dynamic'
+
+const Navbar = dynamic(() => import('@/components/Navbar1'), { ssr: false })
+const Hero = dynamic(() => import('@/components/Hero'), { ssr: false })
+const HowItWorks = dynamic(() => import('@/components/HowItWorks'), { ssr: false })
+const WhyChooseUs = dynamic(() => import('@/components/WhyChooseUs'), { ssr: false })
+const Testimonials = dynamic(() => import('@/components/Testimonials'), { ssr: false })
+const ContactForm = dynamic(() => import('@/components/ContactForm'), { ssr: false })
 
 export default function Home() {
   return (
     <main>
-      <Navbar/>
-      
+      <Navbar />
       <Hero />
       <HowItWorks />
       <WhyChooseUs />
